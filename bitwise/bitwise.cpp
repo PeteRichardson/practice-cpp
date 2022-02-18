5dbc388829c145fd3328b240c8fbb285a2b4124b
Pete Richardson <pete@peterichardson.com>
2022-02-18 09:40:21 -0800

    clean up logic and improve comments.

--- bitwise/bitwise.cpp
+++ bitwise/bitwise.cpp
@@ -13,17 +13,25 @@ int main(int argc, char** argv) {
 
     auto mask { 0xFFULL };
     for (auto byte {1}; byte < 9; byte++ ) {
+        // print mask bits with ' 's instead of '0's.
         std::string mask_str { Bits{mask}.to_string() };
         std::replace(mask_str.begin(), mask_str.end(), '0', ' ');
         std::cout << "byte" << byte << " mask = " << mask_str << std::endl;
+
+        // print result bits (hide 0's outside the mask area)
         std::string result { Bits{value & mask}.to_string() };
-        auto prefix_length = (8-byte) * 8;
-        std::string prefix(prefix_length, ' ');
-        auto len = 64 - (prefix_length + 8);
-        std::string suffix(len, ' ');
-        result.replace(0, prefix_length, prefix);
-        result.replace(prefix_length+8, 64-prefix_length+8, suffix);
+        // - replace prefix... i.e. beginning of string up to masked area
+        //   (remember: string::replace takes a start & count, not a start & end!)
+        auto const prefix_end = (8-byte) * 8;     // position right before masked area
+        std::string const prefix(prefix_end, ' ');
+        result.replace(0, prefix.length(), prefix);
+        
+        // - replace suffix... i.e. chars after mask up to end of string
+        auto const suffix_start = prefix_end + 8; // position right after masked area
+        std::string const suffix(64 - suffix_start, ' ');
+        result.replace(suffix_start, suffix.length(), suffix);
         std::cout << "          -> " << result << std::endl;
+
         mask <<= 8;
     }
     std::cout << "value      = " << Bits{value} << std::endl;
