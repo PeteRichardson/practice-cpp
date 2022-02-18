2f74a7c6f47f041608435f50e0416ce9cdfc3866
Pete Richardson <pete@peterichardson.com>
2022-02-18 14:43:20 -0800

    Use std::getline and specify filename on cmd line

--- files/files.cpp
+++ files/files.cpp
@@ -3,14 +3,31 @@
 
 int main (int argc, char** argv) {
     if (argc < 2) {
-        std::cout << "# usage: files <filename> [<filename>...]" << std::endl;
+        std::cout << "# usage: files <filename>" << std::endl;
         return 1;
     }
-    // open the stream and create iterators on it
-    std::ifstream ifs("foo.txt");
-    std::istream_iterator<std::string> begin(ifs), end;
+
+    std::ifstream file(argv[1]);
+    if (file.is_open()) {
     
-    std::for_each(begin, end, [&](std::string token){ 
-        std::cout << token << std::endl;
-    });
-}
+        // METHOD 1: using std::getline in a loop
+        std::string line{};
+        while (std::getline(file, line)) {
+            std::cout << line << std::endl;
+        }
+
+        file.clear();  // need to reset the stream before METHOD 2
+        file.seekg(0);
+
+        // METHOD 2:  using istream_iterator<std::string>
+        //            Note that this returns tokens, not lines
+        std::istream_iterator<std::string> begin(file), end;
+        std::for_each(begin, end, [&](std::string token){ 
+            std::cout << token << std::endl;
+        });
+
+    }
+    file.close();
+
+
+ }
