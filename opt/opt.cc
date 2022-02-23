1d46222eac105ce5ec84ff8e05d2a6ac7e447ee3
Pete Richardson <pete@peterichardson.com>
2022-02-23 12:47:25 -0800

    iterate over a vector of ints.
    Wow!  Impressive!

--- opt/opt.cc
+++ opt/opt.cc
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using std::cout, std::endl, std::string;
 
 int main(int argc, char** argv) {
-    cout << "Hello, World!" << endl;
+    std::vector<int> coll {};
+    for (int i=24; i<=30; i++) {
+        coll.push_back(i);
+    }
+
+    for (int j=0; j<coll.size(); j++) {
+        cout << coll[j] << " ";
+    }
+    cout << endl;
 }
