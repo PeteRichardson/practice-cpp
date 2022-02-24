56373cf8ab02c6846dbd7ac70711edc21053aea3
Pete Richardson <pete@peterichardson.com>
2022-02-24 12:45:30 -0800

    Change to std::set and try out merge()

--- collections/collections.cc
+++ collections/collections.cc
@@ -1,14 +1,25 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <set>
 
+using std::cout, std::endl;
 
-using std::cout, std::endl, std::string;
+using strings = std::set<std::string>;
 
 int main(int argc, char** argv) {
-    std::vector<string> vec { "Pete", "Wendy", "Katherine", "Bella" };
+    strings people { "Pete", "Wendy", "Katherine"};
+    strings pets { "Bella", "Newton", "Zoe"};
 
-    for (const string &s: vec) {
+    strings family {};
+    family.merge(people);
+    family.merge(pets);
+
+    for (auto &s: family) {
         cout << s << endl;
     }
+
+    cout << family.size() << endl;
+
+    cout << "Bella in the set? " << family.contains("Bella") << endl;
+
 }
