e4db7a932ae346715251b01c9fe9755747f28e8e
Pete Richardson <pete@peterichardson.com>
2022-02-26 13:02:27 -0800

    Collections: switch to shared_ptrs.  No copying!

--- collections/collections.cc
+++ collections/collections.cc
@@ -9,14 +9,16 @@
 using std::cout, std::endl, std::string;
 
 int main(int argc, char** argv) {
-    Being pete{"Pete Richardson", 56, 'M', 2, "xxx-xx-1234"};
-    Human wendy{"Wendy Wilson", 55, 'F', 2, "xxx-xx-5678", "650-218-7915"};
-    Animal bella{"Bella", 8, 'F', 4, "zzz-zzz-4321", "Russian Blue"};
+
+    auto pete = std::make_shared<Being>("Pete Richardson", 56, 'M', 2, "xxx-xx-1234");
+    auto wendy = std::make_shared<Human>("Wendy Wilson", 55, 'F', 2, "xxx-xx-5678", "650-218-7915");
+    auto bella = std::make_shared<Animal>("Bella", 8, 'F', 4, "zzz-zzz-4321", "Russian Blue");
     cout << endl;
 
-    std::vector<Being> people { std::move(pete), std::move(wendy), std::move(bella) };
-    for (Being p: people ) {
-        cout << p << endl;
+    std::vector<std::shared_ptr<Being>> people { pete, wendy, bella };
+    for (auto p: people ) {
+        cout << *p << endl;
     }
+    cout << endl;
 
 }
