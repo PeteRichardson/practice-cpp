ff95ed62d312277b15f811f21d02485a9038e746
Pete Richardson <pete@peterichardson.com>
2022-02-26 12:02:47 -0800

    Collections: add destrutors and copy constructors that log

--- collections/collections.cc
+++ collections/collections.cc
@@ -12,12 +12,18 @@ public:
 
     Being(string name, unsigned int age, char gender, unsigned int leg_count={}, string ss_number={"000-00-0000"}) :
         name_{name}, age_{age}, gender_{gender}, leg_count_{leg_count}, ss_number_{ss_number} {
-        cout << "# Created Being " << name_ << endl;
+        cout << "# Created Being " << name_ << std::hex << "@" << this << endl;
     }
 
-    // friend std::ostream& operator<<(std::ostream& out, const Being& b) {
-    //     return b.print(out);
-    // }
+    // Copy constructor that logs copies
+    Being(const Being& rhs) : name_{rhs.name_}, age_{rhs.age_}, gender_{rhs.gender_},
+        leg_count_{rhs.leg_count_}, ss_number_{rhs.ss_number_} {
+            cout << "# Copying Being " << name_ << std::hex << "@" << this << endl;   
+    }
+
+    ~Being() {
+        cout << "# Destroyed Being " << name_ << std::hex << "@" << this << endl;
+    }
 
     virtual std::ostream& print(std::ostream& out) const {
         out << "Being("; dump_fields(out); out << ")";
@@ -31,6 +37,10 @@ public:
 
 private:
     string ss_number_ {};
+    // Being( const Being& rhs ) = delete;          // No need to delete the copy constructor.
+                                                    // It's manually defined above
+    Being& operator=( const Being& rhs ) = delete;
+
 };
 
 
@@ -45,7 +55,11 @@ public:
 
     Human(string name, unsigned int age, char gender, unsigned int leg_count=2, string ss_number="000-00-0000", string phone_number="") :
         Being(name, age, gender, leg_count, ss_number), phone_number_{phone_number} {
-        cout << "# Created Human: " << name_ << endl;
+        cout << "# Created Human: " << name_ << std::hex << "@" << this << endl;
+    }
+
+    ~Human() {
+        cout << "# Destroyed Human " << name_ << std::hex << "@" << this << endl;
     }
 
     std::ostream& print(std::ostream& out) const override {
@@ -67,7 +81,16 @@ public:
 
     Animal(string name, unsigned int age, char gender, unsigned int leg_count=4, string ss_number="111-11-1111", string breed="") :
         Being(name, age, gender, 4, ss_number), breed_{breed} {
-        cout << "# Created Animal " << name_ << endl;
+        cout << "# Created Animal " << name_ << std::hex << "@" << this << endl;
+    }
+
+    // Copy constructor that logs copies
+    Animal(const Animal& rhs) : Being{rhs}, breed_{rhs.breed_} {
+            cout << "# Copying Animal " << name_ << std::hex << "@" << this << endl;   
+    }
+
+    ~Animal() {
+        cout << "# Destroyed Animal " << name_ << std::hex << "@" << this << endl;
     }
 
     std::ostream& print(std::ostream& out) const override {
@@ -90,6 +113,10 @@ int main(int argc, char** argv) {
    
     cout << pete << endl;
     cout << wendy << endl;
-    cout << bella << endl;
+    // cout << static_cast<Being&&>(bella) << endl;        // Copy constructor not called (rval ref => no copy)
+    cout << static_cast<Being&>(bella) << endl;      // Copy constructor not called (ref => no copy)
+    //cout << static_cast<Being>(bella) << endl;       // Copy constructor called
+    // cout << bella << endl;                           // Copy constructor not called (no cast => no copy).
+    cout << endl;
 
 }
