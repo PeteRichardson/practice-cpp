a6d482723dd094cac61f9e226cb2dcc58a8e6e5e
Pete Richardson <pete@peterichardson.com>
2022-02-26 12:52:43 -0800

    Collections: Add objects to vector and print.   Makes 3 copies of each! yikes!

--- collections/collections.cc
+++ collections/collections.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "being.h"
 #include "human.h"
@@ -12,13 +13,10 @@ int main(int argc, char** argv) {
     Human wendy{"Wendy Wilson", 55, 'F', 2, "xxx-xx-5678", "650-218-7915"};
     Animal bella{"Bella", 8, 'F', 4, "zzz-zzz-4321", "Russian Blue"};
     cout << endl;
-   
-    cout << pete << endl;
-    cout << wendy << endl;
-    // cout << static_cast<Being&&>(bella) << endl;  // Copy constructor not called (rval ref => no copy)
-    cout << static_cast<Being&>(bella) << endl;      // Copy constructor not called (ref => no copy)
-    //cout << static_cast<Being>(bella) << endl;     // Copy constructor called
-    // cout << bella << endl;                        // Copy constructor not called (no cast => no copy).
-    cout << endl;
+
+    std::vector<Being> people { std::move(pete), std::move(wendy), std::move(bella) };
+    for (Being p: people ) {
+        cout << p << endl;
+    }
 
 }
