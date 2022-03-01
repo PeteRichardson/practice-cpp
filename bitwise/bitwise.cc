8fb9e592c3a200de98f21fac836be163958aba59
Pete Richardson <pete@peterichardson.com>
2022-02-28 16:27:29 -0800

    Bitwise: Add <algorithm> header for std::replace.
    Clean up using clang-format

--- bitwise/bitwise.cc
+++ bitwise/bitwise.cc
@@ -1,5 +1,6 @@
-#include <iostream>
+#include <algorithm>
 #include <bitset>
+#include <iostream>
 #include <string>
 
 #include "utils.h"
@@ -13,30 +14,31 @@ unsigned long long value { 0x1234567812345678ULL};
 int main(int argc, char** argv) {
     log("Hello, Bitwise!");
 
-    cout << "value      = " << Bits{value} << endl;
+      cout << "value      = " << Bits{value} << endl;
 
-    auto mask { 0xFFULL };
-    for (auto byte {1}; byte <= 8; byte++ ) {
-        // print mask bits with ' 's instead of '0's.
-        string mask_str { Bits{mask}.to_string() };
-        std::replace(mask_str.begin(), mask_str.end(), '0', ' ');
-        cout << "byte" << byte << " mask = " << mask_str << endl;
+  auto mask{0xFFULL};
+  for (auto byte{1}; byte <= 8; byte++) {
+    // print mask bits with ' 's instead of '0's.
+    string mask_str{Bits{mask}.to_string()};
+    std::replace(mask_str.begin(), mask_str.end(), '0', ' ');
+    cout << "byte" << byte << " mask = " << mask_str << endl;
 
-        // print result bits (hide 0's outside the mask area)
-        string result { Bits{value & mask}.to_string() };
-        // - replace prefix... i.e. beginning of string up to masked area
-        //   (remember: string::replace takes a start & count, not a start & end!)
-        auto const prefix_end = (8-byte) * 8;     // position right before masked area
-        string const prefix(prefix_end, ' ');
-        result.replace(0, prefix.length(), prefix);
-        
-        // - replace suffix... i.e. chars after mask up to end of string
-        auto const suffix_start = prefix_end + 8; // position right after masked area
-        string const suffix(64 - suffix_start, ' ');
-        result.replace(suffix_start, suffix.length(), suffix);
-        cout << "          -> " << result << endl;
+    // print result bits (hide 0's outside the mask area)
+    string result{Bits{value & mask}.to_string()};
+    // - replace prefix... i.e. beginning of string up to masked area
+    //   (remember: string::replace takes a start & count, not a start & end!)
+    auto const prefix_end = (8 - byte) * 8; // position right before masked area
+    string const prefix(prefix_end, ' ');
+    result.replace(0, prefix.length(), prefix);
 
-        mask <<= 8;
+    // - replace suffix... i.e. chars after mask up to end of string
+    auto const suffix_start =
+        prefix_end + 8; // position right after masked area
+    string const suffix(64 - suffix_start, ' ');
+    result.replace(suffix_start, suffix.length(), suffix);
+    cout << "          -> " << result << endl;
+
+    mask <<= 8;
     }
     cout << "value      = " << Bits{value} << endl;
  
