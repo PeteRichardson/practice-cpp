ca496bffaa9468f2fb4b69ddc2ca2387ff86586b
Pete Richardson <pete@peterichardson.com>
2022-03-04 11:31:50 -0800

    Overall:  Add pretty dump(ostream&, string, T&) function to dbg namespace

--- common/hexdump.h
+++ common/hexdump.h
@@ -93,50 +93,56 @@ inline std::ostream& hex_dump(std::ostream& os, const std::byte* buffer, std::si
 }
 
 namespace dbg {
-class memdump {
-public:
-  memdump(const void* buf, std::size_t bufsz)
-      : buffer_{reinterpret_cast<const std::byte*>(buf)}, bufsize_{bufsz} {}
+  class memdump {
+  public:
+    memdump(const void* buf, std::size_t bufsz)
+        : buffer_{reinterpret_cast<const std::byte*>(buf)}, bufsize_{bufsz} {}
 
-  template <typename T>
-  explicit memdump(const T& buf)
-      : buffer_{reinterpret_cast<const std::byte*>(&buf)}, bufsize_{sizeof(T)} {}
+    template <typename T>
+    explicit memdump(const T& buf)
+        : buffer_{reinterpret_cast<const std::byte*>(&buf)}, bufsize_{sizeof(T)} {}
 
-  // It's UB to access `+ 1`th byte of a string_view so we don't, despite most
-  // targets of string_views (ie std::string or string literal) having '\0'.
-  template <>
-  explicit memdump(const std::string_view& buf)
-      : buffer_{reinterpret_cast<const std::byte*>(&buf)}, bufsize_{sizeof(buf)} {
-    child_       = std::make_unique<memdump>(buf.data(), buf.size());
-    child_label_ = "string viewed";
-  }
-
-  // There is some debate but we believe str[size()] is legal via [] or *
-  // but UB via iterator. So here we DO show the '`0' terminator.
-  template <>
-  explicit memdump(const std::string& buf)
-      : buffer_{reinterpret_cast<const std::byte*>(&buf)}, bufsize_{sizeof(buf)} {
-    auto data_byte_ptr = reinterpret_cast<const std::byte*>(buf.data());
-    if (!(data_byte_ptr > buffer_ && data_byte_ptr < buffer_ + bufsize_)) {
-      // not SBO, show the real string as well
-      child_ = std::make_unique<memdump>(buf.data(), buf.size() + 1);
-      child_label_ = "heap string";
+    // It's UB to access `+ 1`th byte of a string_view so we don't, despite most
+    // targets of string_views (ie std::string or string literal) having '\0'.
+    template <>
+    explicit memdump(const std::string_view& buf)
+        : buffer_{reinterpret_cast<const std::byte*>(&buf)}, bufsize_{sizeof(buf)} {
+      child_       = std::make_unique<memdump>(buf.data(), buf.size());
+      child_label_ = "string viewed";
     }
-  }
 
-  friend std::ostream& operator<<(std::ostream& os, const memdump& memdump) {
-    hex_dump(os, memdump.buffer_, memdump.bufsize_); // NOLINT
-     if (memdump.child_) os << std::setw(19) << memdump.child_label_ << ":\n" << *(memdump.child_);
-    return os;
-  }
+    // There is some debate but we believe str[size()] is legal via [] or *
+    // but UB via iterator. So here we DO show the '`0' terminator.
+    template <>
+    explicit memdump(const std::string& buf)
+        : buffer_{reinterpret_cast<const std::byte*>(&buf)}, bufsize_{sizeof(buf)} {
+      auto data_byte_ptr = reinterpret_cast<const std::byte*>(buf.data());
+      if (!(data_byte_ptr > buffer_ && data_byte_ptr < buffer_ + bufsize_)) {
+        // not SBO, show the real string as well
+        child_ = std::make_unique<memdump>(buf.data(), buf.size() + 1);
+        child_label_ = "heap string";
+      }
+    }
 
-private:
-  const std::byte* buffer_;
-  std::size_t      bufsize_;
+    friend std::ostream& operator<<(std::ostream& os, const memdump& memdump) {
+      hex_dump(os, memdump.buffer_, memdump.bufsize_); // NOLINT
+      if (memdump.child_) os << std::setw(19) << memdump.child_label_ << ":\n" << *(memdump.child_);
+      return os;
+    }
 
-  std::unique_ptr<memdump> child_ = nullptr;
-  std::string         child_label_;
-};
+  private:
+    const std::byte* buffer_;
+    std::size_t      bufsize_;
+
+    std::unique_ptr<memdump> child_ = nullptr;
+    std::string         child_label_;
+  };
+
+    template <typename T>
+    void dump(std::ostream& out, std::string message, T& obj) {
+        out << message << " [size " << sizeof obj << " bytes]:\n";
+        out << dbg::memdump(obj) << std::endl;
+    }
 }
 
 #endif
--- hex/hex.cc
+++ hex/hex.cc
@@ -1,14 +1,15 @@
+#include <any>
 #include <iostream>
 #include <memory>
 
 #include "utils.h"
 #include "person.h"
 
-using std::cout, std::endl;
+using std::cerr, std::endl;
 
 int main(int argc, char** argv) {
     auto pete = std::make_unique<Person>("Pete");
 
-    cout << dbg::memdump(pete) << endl;
-    cout << dbg::memdump(*pete) << endl;
+    dbg::dump(cerr, "pete", pete);
+    dbg::dump(cerr, "*pete", *pete);
 }
