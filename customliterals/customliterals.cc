68f518007b137eee74ead182bd461b652f11de5f
Pete Richardson <pete@peterichardson.com>
2022-03-05 11:43:04 -0800

    Customliterals:  Add logging and refactor

--- customliterals/customliterals.cc
+++ customliterals/customliterals.cc
@@ -1,20 +1,37 @@
 #include <iostream>
 #include <string>
 
+#include "log.h"
+#include "dbg.h"
 #include "person.h"
 
 using std::cout, std::endl, std::string;
 
+void DoPerson(Person& p, string message = "") {
+    PLOGI << "Creating " << message;
+    PLOGD << "&p\n" << dbg::memdump(&p);
+    PLOGD << "p\n" << dbg::memdump(p);   
+    cout << p << endl;
+
+}
+
+void DoPerson(Person&& p, string message = "") {
+    PLOGV << "Calling through DoPerson(Person&&, string)";
+    DoPerson(p, message);
+}
+
 int main(int argc, char** argv) {
-    Person pete { "Pete", 56, 'M'};
-    cout << pete << endl;
+    setup_console_logging(plog::warning);
 
+    PLOGV << "BEGIN";
+
+    DoPerson(Person("Pete", 56, 'M'), R"|(Person pete {"Pete", 56, 'M'}")|");
+    DoPerson(Person("Wendy"), R"|(Person("Wendy"))|");
+   
     auto wendy = "Wendy"_person;
-    cout << wendy << endl;
-
-    auto wendy2 = Person("Wendy");
-    cout << wendy2 << endl;
-
+    DoPerson(wendy, R"|("Wendy"_person)|");
+    
     auto ian = "Ian"_person;
-    cout << ian << endl;
+    DoPerson(ian, R"("Ian"_person)");
+    PLOGV << "END";
 }
