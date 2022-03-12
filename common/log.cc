c1316461fd811ede9e965e8b7374ae74afcf86ee
Pete Richardson <pete@peterichardson.com>
2022-03-12 11:36:20 -0800

    Common:  switch PLOG logging to stderr from stdout

--- common/log.cc
+++ common/log.cc
@@ -1,7 +1,6 @@
 #include "log.h"
 
 void setup_console_logging(plog::Severity sev=plog::debug) {
-    static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
-    plog::init(plog::verbose, &consoleAppender);
-    plog::get()->setMaxSeverity(sev);
+    static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender(plog::streamStdErr);
+    plog::init(sev, &consoleAppender);
 }
