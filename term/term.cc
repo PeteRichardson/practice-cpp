648213e6af665148d3d0acade0dcbad6111fd3af
Pete Richardson <pete@peterichardson.com>
2022-03-13 13:11:11 -0700

    Term:  Simple test of cpp-terminal used with plog

---
+++ term/term.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cpp-terminal/base.hpp>
+
+#include <log.h>
+
+int main(int argc, char** argv) {
+    setup_console_logging(plog::debug);
+    PLOGN << Term::color(Term::fg::yellow) << "Hello, World!";
+    PLOGD << "This shouldn't be yellow (if plot or cpp-terminal reset the terminal colors automatically)";
+}
