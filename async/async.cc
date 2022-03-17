c8742b2b29eb06f994adcae300f4efac9b2f0399
Pete Richardson <pete@peterichardson.com>
2022-03-17 11:05:27 -0700

    async: use syncstream on linux instead of mutex

--- async/async.cc
+++ async/async.cc
@@ -5,22 +5,34 @@
 #include <mutex>
 #include <random>
 
+#if defined(__unix__) 
+#include <syncstream>
+#elif defined(__APPLE__) && defined(__MACH__) // Apple OSX and iOS (Darwin)
+std::mutex coutMutex{};
+#endif
+
 #include <common/log.h>
 #include <common/perf.h>
 #include <common/dbg.h>
 
 using std::cout, std::endl, std::async;
 
-std::mutex coutMutex;
-
 int DoSomething(int n) {
     std::random_device rd{};
 
     for (int i=0; i<10; ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(rd() % 1000));
+
+        // On linux we can use c++20 std::osyncstream to wrap cout and make sure the output is 
+        // not interleaved.   On Mac (for now), <syncstream> isn't available, so use a mutex
+        // and lock_guard.
+        // TODO:  What about Windows?
+#if defined(__unix__) 
+        std::osyncstream(std::cout) << std::hex << std::noshowbase << std::setw(2) << n << std::endl;
+#elif defined(__APPLE__) && defined(__MACH__) // Apple OSX and iOS (Darwin)
         std::lock_guard<std::mutex> coutLock(coutMutex);
-        std::cout << std::hex << std::noshowbase << std::setw(2) << n << '\n';
-        std::cout.flush();
+        std::cout << std::hex << std::noshowbase << std::setw(2) << n << std::endl;
+#endif
     }
     return n;
 }
