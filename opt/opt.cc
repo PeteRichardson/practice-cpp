66693dd039dfa2777f35ac411c7856c568d19851
Pete Richardson <pete@peterichardson.com>
2022-03-16 18:28:02 -0700

    opt: put args into a std::span<char*> for no useful reason

--- opt/opt.cc
+++ opt/opt.cc
@@ -1,17 +1,21 @@
 #include <iostream>
-#include <string>
-#include <array>
+#include <span>
 
-using std::cout, std::endl, std::string;
+using std::cout, std::endl;
 
-int main(int argc, char** argv) {
-    std::array<long long,3> arr {};
-    for (int i=0; i<=5; i++) {  // overwrite end of array!  (5 > 3)
-        arr[i] = i + 16LL;
-    }
+int main(int argc, char* argv[]) {
 
-    for (int j=0; j<arr.size(); j++) {
-        cout << arr[j] << " " << std::flush;
-    }
-    cout << endl;
+	if (argc == 1) {
+		cout << "# usage: " << argv[0] << " <word>..." << endl;
+		return(EXIT_FAILURE);
+	}
+
+    // put args into a std::span<char*>.   Not really necessary.
+	auto args = std::span<char*>{++argv, static_cast<size_t>(--argc)};
+    for (auto arg : args) {
+		cout << arg << " is " << static_cast<std::string_view>(arg).length() << " chars long." << endl;
+	}
+    cout << "argv[0] = " << argv[0] << endl;
+    cout << "args[1] = " << args[1] << endl;
+	return(EXIT_SUCCESS);
 }
