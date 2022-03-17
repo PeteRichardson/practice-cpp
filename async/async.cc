010576fad53f0ac549a748377a9044137922d97e
Pete Richardson <pete@peterichardson.com>
2022-03-17 10:12:52 -0700

    async: add mutex to guard against interleaving cout chars
    Adds a few hundred ms to a 50 thread run.
    ~6300ms to ~6700ms on this M1 Max macbook pro

--- async/async.cc
+++ async/async.cc
@@ -11,11 +11,14 @@
 
 using std::cout, std::endl, std::async;
 
+std::mutex coutMutex;
+
 int DoSomething(int n) {
     std::random_device rd{};
 
     for (int i=0; i<10; ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(rd() % 1000));
+        std::lock_guard<std::mutex> coutLock(coutMutex);
         std::cout << std::hex << std::noshowbase << std::setw(2) << n << '\n';
         std::cout.flush();
     }
